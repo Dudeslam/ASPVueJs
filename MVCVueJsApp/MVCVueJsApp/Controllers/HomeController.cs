b1ba3cf601c944799740a41da366ee3ad5516e6e
Vinh Trung Thai <vtt@bytelab.dk>
2022-03-24 12:56:35 +0100

    First project for VueJs Razor pages

---
+++ MVCVueJsApp/MVCVueJsApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MVCVueJsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCVueJsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new IndexViewModel
+            {
+                User = new User()
+                {
+                    LastName = "Peterson",
+                    Name = "Peter",
+                    Id = "2"
+                }
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
