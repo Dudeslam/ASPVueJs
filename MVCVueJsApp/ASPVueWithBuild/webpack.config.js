7b1b063fc60bd07986dd7d6c3f08693c4055d673
Vinh Trung Thai <vtt@bytelab.dk>
2022-03-29 10:24:56 +0200

    Method Example of VueJs

---
+++ MVCVueJsApp/ASPVueWithBuild/webpack.config.js
@@ -0,0 +1,7 @@
+﻿const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssestsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
